ad3284ac6021833a688b39860d316b621098378c
Carson Swoveland <sciencedude2003@gmail.com>
2020-01-24 20:01:17 -0700

    Initial commit

---
+++ src/camera.rs
@@ -0,0 +1,6 @@
+use crate::physical::Vector2;
+
+#[derive(Debug, Clone)]
+pub struct Camera {
+    pub position: Vector2,
+}
---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub mod animation;
+pub mod physical;
+pub mod asset_mgr;
+pub mod camera;
+pub mod renderable;
+pub mod input;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
